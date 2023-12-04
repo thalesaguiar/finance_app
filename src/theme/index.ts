0dc1c8146c5a559e99e87945a72c1b65677882f7
Thales Aguiar <dev.thalesaguiar@gmail.com>
2023-12-03 21:47:33 -0300

    feat: created app theme

---
+++ src/theme/index.ts
@@ -0,0 +1,40 @@
+    
+    const Theme = {
+
+        COLORS: {
+            WHITE: '#FFFFFF',
+            WHITE_100: '#EFEFED', 
+
+            GREEN_700: '#00875F',
+            GREEN_500: '#00B37E',
+
+            BLUE: '#788FDB',
+        
+            RED: '#F75A68',
+            RED_DARK: '#AA2834',
+        
+            GRAY_700: '#121214',
+            GRAY_600: '#202024',
+            GRAY_500: '#29292E',
+            GRAY_400: '#323238',
+            GRAY_300: '#7C7C8A',
+            GRAY_200: '#C4C4CC',
+            GRAY_100: '#E1E1E6',
+        },
+
+        FONT_FAMILY: {
+            REGULAR: 'Roboto_400Regular',
+            BOLD: 'Roboto_700Bold',
+        },
+
+        FONT_WEIGHT: {
+            SM: 14,
+            MD: 16,
+            LG: 18,
+            XL: 24
+        }
+
+  }
+
+
+export default Theme;
