7b8a7d4ad566536514307f5bfab2384da634df6b
Thales Aguiar <dev.thalesaguiar@gmail.com>
2023-12-06 23:54:25 -0300

    feat: add new routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,23 +1,21 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import { HomeScreen } from '@screens/HomeScreen';
-import { Login } from '@screens/Login';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SiginScreen } from "@screens/SiginScreen";
+import { HomeScreen } from "@screens/HomeScreen";
+import { Login } from "@screens/Login/Login";
+import { CreateAcount } from "@screens/CreateAcountScreen";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes() {
-  return(
-    <Navigator screenOptions={{headerShown: false}} >
-      <Screen
-        name='Login'
-        component={Login}
-      />
-      
-      <Screen
-        name='HomeScreen'
-        component={HomeScreen}
-      />
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="Login" component={Login} />
 
+      <Screen name="SigIn" component={SiginScreen} />
+
+      <Screen name="CreateAcount" component={CreateAcount} />
+
+      <Screen name="HomeScreen" component={HomeScreen} />
     </Navigator>
-  )
-} 
+  );
+}
