e86eb669d8ce1dda5d8208d1f14d72f6912f9e45
Thales Aguiar <dev.thalesaguiar@gmail.com>
2023-12-11 21:47:25 -0300

    feat: prevented glitch navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import { HomeScreen } from "@screens/HomeScreen";
-import { Login } from "@screens/Login";
-import theme from "./src/theme";
+import { Login } from "@screens/Login/Login";
+import { theme } from "./src/theme";
 import { ThemeProvider } from "styled-components/native";
 import { Routes } from "@routes/index";
+import { SiginScreen } from "@screens/SiginScreen";
 
 export default function App() {
   return (
-
     <ThemeProvider theme={theme}>
       <Routes />
     </ThemeProvider>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,16 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { AppRoutes } from './app.routes';
+import { NavigationContainer } from "@react-navigation/native";
+import { AppRoutes } from "./app.routes";
+import { View } from "react-native";
+import { useTheme } from "styled-components";
 
 export function Routes() {
-  return(
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
-  )
+  const { COLORS } = useTheme();
+
+  return (
+    <View style={{ flex: 1, backgroundColor: COLORS.WHITE_100 }}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
+  );
 }
