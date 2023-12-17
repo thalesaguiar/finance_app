9f0256c7610a28fba6712975f4f4c0d32b4490ef
Thales Aguiar <dev.thalesaguiar@gmail.com>
2023-12-16 23:18:52 -0300

    feat: new styles add

--- src/components/BanksList/styles.ts
+++ src/components/BanksList/styles.ts
@@ -1,13 +1,18 @@
 import styled from 'styled-components/native'
 import { theme } from "../../theme";
+import ReactNativeModal from 'react-native-modal';
 
+export type BackButtonContainerStylePorps = 'PRIMARY' | 'SECONDARY';
+
+type Props = {
+  type: BackButtonContainerStylePorps;
+}
 
 export const Container = styled.View`
-  flex: 1;
   width: 100%;
   justify-content: center;
   align-items: center;
-`
+  `
 
 export const ContentWrapper = styled.View`
   background-color: ${theme.COLORS.WHITE};
@@ -37,14 +42,33 @@ export const BackButtonContainer = styled.View`
   width: 100%;
   height: 55px;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-evenly;
   flex-direction: row ;
   padding: 10px;
   `
+
+export const BackButtonContainerB = styled.View`
+  flex: 1;
+  background-color: ${theme.COLORS.WHITE_100};
+  width: 100%;
+  flex-direction: row;
+  padding: 10px;
+`
  
 export const TopTitle = styled.Text`
 font-size: ${theme.FONT_SIZE.LG}px;
 font-weight: 600;
-color: ${theme.COLORS.WHITE};
+color: ${theme.COLORS.GREEN_400};
 margin-right: 200px;
+`
+
+export const TopTitle2 = styled.Text`
+font-size: ${theme.FONT_SIZE.LG}px;
+font-weight: 600;
+color: ${theme.COLORS.GRAY_500};
+margin-right: 200px;
+`
+
+export const ModalB = styled(ReactNativeModal)`
+
 `
--- src/components/GoBackButton/styles.ts
+++ src/components/GoBackButton/styles.ts
@@ -11,5 +11,5 @@ export const BackButton = styled.TouchableOpacity`
 
 export const BackIcon = styled(AntDesign).attrs(({ theme }) => ({
   size: 32,
-  color: theme.COLORS.WHITE,
+  color: theme.COLORS.GRAY_600,
 }))``;
