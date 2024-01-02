bef35ff1677d4c4f729c46c9b3e8fe6660528707
thalesaguiar <dev.thalesaguiar@gmail.com>
2024-01-01 21:29:30 -0300

    refactor: remove stylesheet and add styled component

--- src/components/profile-status/index.tsx
+++ src/components/profile-status/index.tsx
@@ -1,79 +1,16 @@
-import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import { Feather } from "@expo/vector-icons";
-import { Container } from "./styles";
+import { Container, WrappProfile, Menssage, ProfilePic } from "./styles";
 
 export function ProfileStats() {
   return (
     <Container>
-      <View style={styles.imageTextWrapper}>
-        <TouchableOpacity>
-          <View style={styles.imageWrapper}>
-            <Image
-              source={require("@assets/user.png")}
-              style={styles.profileImage}
-            />
-          </View>
-        </TouchableOpacity>
-
-        <View style={styles.nameWrapper}>
-          <Text style={styles.profileText}>Olá,</Text>
-          <Text style={styles.profileNameText}>Thales</Text>
-        </View>
-      </View>
-
-      <TouchableOpacity>
-        <View style={styles.modalButtonWrapper}>
-          <Feather name={"bell"} size={35} color={"#E7F3E0"} />
-        </View>
-      </TouchableOpacity>
+      <WrappProfile>
+        <ProfilePic>
+          <Feather name="user" color={"#47C66F"} size={30} />
+        </ProfilePic>
+        <Menssage>Bem Vindo</Menssage>
+      </WrappProfile>
+      <Feather color={"#fff"} name="bell" size={36} />
     </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  imageWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: "#E7F3E0",
-    borderRadius: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginRight: 10,
-    marginLeft: 20,
-  },
-  profileImage: {
-    width: 40,
-    height: 40,
-  },
-  nameWrapper: {
-    flexDirection: "column",
-  },
-  profileText: {
-    color: "#E7F3E0",
-    fontSize: 16,
-    marginLeft: 5,
-  },
-  profileNameText: {
-    color: "#E7F3E0",
-    fontSize: 20,
-    fontWeight: "bold",
-    marginLeft: 5,
-  },
-  modalButtonWrapper: {
-    width: 50,
-    height: 50,
-    backgroundColor: "#149E40",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10,
-    marginRight: 20,
-  },
-  ts: {
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-  imageTextWrapper: {
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-});
